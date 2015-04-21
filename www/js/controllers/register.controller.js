2d52b181c7090573503cb32afad7595f915f94e0
Rafael Grillo <grillorafael@gmail.com>
2015-04-21 10:47:06 -0300

    added register form validation

--- www/js/controllers/register.controller.js
+++ www/js/controllers/register.controller.js
@@ -3,17 +3,23 @@
     angular.module('zaptv').controller('RegisterCtrl', RegisterCtrl);
 
     function RegisterCtrl($scope, $state, $ionicPlatform, $ionicHistory, User, Auth) {
-        $scope.register = function(user) {
-            User.register(user).then(function() {
-                $ionicHistory.nextViewOptions({
-                    disableBack: true,
-                    historyRoot: true
-                });
+        $scope.setForm = function(f) {
+            $scope.form = f;
+        };
 
-                $state.go('channels');
-            }, function() {
-                // TODO Handle error
-            });
+        $scope.register = function(user) {
+            if($scope.form.registerForm.$valid) {
+                User.register(user).then(function() {
+                    $ionicHistory.nextViewOptions({
+                        disableBack: true,
+                        historyRoot: true
+                    });
+
+                    $state.go('channels');
+                }, function() {
+                    // TODO Handle error
+                });
+            }
         };
     }
 })();
