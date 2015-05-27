ef6baf3b3aa537f1aea0120d268ac634d6d98380
Ricardo Goldstein <goldstein.ricardo@gmail.com>
2015-05-27 00:40:16 -0300

    add validation error msg in register form

--- www/js/controllers/register.controller.js
+++ www/js/controllers/register.controller.js
@@ -2,7 +2,7 @@
     'use strict';
     angular.module('zaptv').controller('RegisterCtrl', RegisterCtrl);
 
-    function RegisterCtrl($scope, $state, $ionicPlatform, $ionicLoading, $ionicHistory, $animationTrigger, User, Auth, Analytics) {
+    function RegisterCtrl($scope, $state, $ionicPlatform, $ionicLoading, $ionicHistory, $animationTrigger, User, Auth, Analytics, ngNotify) {
         $scope.$on('$ionicView.enter', function() {
             $ionicPlatform.ready(function() {
                 Analytics.init();
@@ -16,10 +16,26 @@
 
         $scope.register = function(user) {
             $ionicLoading.show({
-              template: 'Carregando...',
-              hideOnStateChange: true
+                template: 'Carregando...',
+                hideOnStateChange: true
             });
-            if($scope.form.registerForm.$valid) {
+            var msg = '';
+            if (!user || !$scope.form.registerForm.username.$valid) {
+                msg = 'Usuário necessita ter no mínimo 4 caracteres.';
+                console.log(user)
+                if (user && $scope.form.registerForm.username.$modelValue.length >= 4) {
+                    msg = 'Este usuário já existe. Escolha outro, por favor.'
+                }
+            }
+
+            else if(!$scope.form.registerForm.email.$valid) {
+                msg = 'O email necessita ser válido.';
+            }
+            // else if($scope.form.registerForm.password.$modelValue.length < 4) {
+            //     msg = 'A senha necessita ser mais que 4 dígitos.';
+            // }
+
+            if ($scope.form.registerForm.$valid) {
                 User.register(user).then(function(tokenData) {
                     Analytics.trackEvent('Auth', 'register');
                     $ionicHistory.nextViewOptions({
@@ -33,9 +49,12 @@
                     $ionicLoading.hide();
                     $animationTrigger.trigger('register-form', 'bounce-finite', $animationTrigger.FROM_START);
                 });
-            }
-            else {
+            } else {
                 $ionicLoading.hide();
+                ngNotify.set(msg, {
+                    position: 'bottom',
+                    type: 'error'
+                });
                 $animationTrigger.trigger('register-form', 'bounce-finite', $animationTrigger.FROM_START);
             }
         };
