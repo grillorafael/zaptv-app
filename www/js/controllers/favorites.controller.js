fc0a5d05193423738640eb5537938b4410afa1ec
Ricardo Goldstein <goldstein.ricardo@gmail.com>
2015-06-02 21:41:10 -0300

    add favorites show view and modal

--- www/js/controllers/favorites.controller.js
+++ www/js/controllers/favorites.controller.js
@@ -2,7 +2,21 @@
     'use strict';
     angular.module('zaptv').controller('FavoritesCtrl', FavoritesCtrl);
 
-    function FavoritesCtrl($scope) {
+    function FavoritesCtrl($scope, $ionicModal) {
+        $ionicModal.fromTemplateUrl('templates/see_shows_modal.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function(modal) {
+            $scope.seeShowsModal = modal;
+        });
+        $scope.seeShows = function() {
+            $scope.seeShowsModal.show().then(function() {
+            });
+            // Analytics.trackEvent('Chat', 'view_schedules');
+        };
 
+        $scope.closeShowsModal = function() {
+            $scope.seeShowsModal.hide();
+        };
     }
 })();
